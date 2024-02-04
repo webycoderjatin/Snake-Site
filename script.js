76fd02bbd7456fd5c704f6c5cdfcd84b1d5713c7
Jatin Rai <jotinrai106@gmail.com>
2024-02-04 10:44:05 +0530

    Snake Site

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function myFunction() {
     }
   })
 }
+
+
+let sr1 = ScrollReveal({
+  distance : '65px',
+  duration : 2600 , 
+  delay :450,
+  reset : false
+})
+let sr2 = ScrollReveal({
+  distance : '65px',
+  duration : 2600 , 
+  delay :450,
+  reset : false
+})
+
+sr1.reveal(".right",{delay:200,origin:"top"});
+sr1.reveal(".hero-image",{delay:300,origin:"top"});
+sr2.reveal(".sec",{delay:300,origin:"top"});
